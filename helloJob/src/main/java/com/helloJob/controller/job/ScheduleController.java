4cfb7e15d99d7468bf98808d05a784914ba2a466
caihaimin <caihm@chinatelecom.cn>
2019-01-11 15:13:59 +0800

    x

--- helloJob/src/main/java/com/helloJob/controller/job/ScheduleController.java
+++ helloJob/src/main/java/com/helloJob/controller/job/ScheduleController.java
@@ -120,8 +120,8 @@ public class ScheduleController extends BaseController {
 				allTriggerJobs.add(jobId);
 				logger.info("将要kill掉的作业:"+allTriggerJobs);
 				String firstLineLog = "<div style='color:red'>"+DateUtils.getCreateTime()+ getStaffName()+"点击运行一次作业"+jobId+"，引起其子孙作业被kill掉 !</div><br>";
-				scheBasicInfoService.killJobs(allTriggerJobs,dt,firstLineLog);
-				jobInstanceService.delete(allTriggerJobs, dt);
+				//scheBasicInfoService.killJobs(allTriggerJobs,dt,firstLineLog);
+				//jobInstanceService.delete(allTriggerJobs, dt);
 				scheBasicInfoService.runOnce(jobId, dt,isSelfRely);
 				ThreadUtils.sleeep(300);
 				return renderSuccess();
